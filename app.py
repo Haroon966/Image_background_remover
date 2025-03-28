abc5f2a48d5c4c74d96fe95c46fd16c8dc4c300a
Haroon Ali <haroon.orenda@gmail.com>
2025-03-28 14:25:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,7 @@ def remove_background():
     return jsonify({'error': 'File type not allowed'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+    
